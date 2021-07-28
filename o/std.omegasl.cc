0c75d27ea75370014445d677914113578f1cbe75
Alex Topper <42527102+Pedestria@users.noreply.github.com>
2021-07-27 20:58:23 -0400

    remove embedded shader lib test

--- o/std.omegasl.cc
+++
@@ -1,7 +0,0 @@
-// Warning. This file was created by omegaslc. DO NOT EDIT!
-#include"std.omegasl.h"
-extern const char __std_metallib[];
-void std_omegasl_load(std::unordered_map<std::string,omegasl_lib_entry> & out){
-out.insert(std::make_pair("__std_metallib",
-{"__std_metallib",__std_metallib,{"coloredVertexShader","coloredFragmentShader",},2}));
-}
--- o/std.omegasl.h
+++
@@ -1,5 +0,0 @@
-// Warning. This file was created by omegaslc. DO NOT EDIT!
-#include "omegasl.h"
-#include <string>
-#include <unordered_map>
-void std_omegasl_load(std::unordered_map<std::string,omegasl_lib_entry> & out);

2d8b68a653198f730cbcbec3f39aaa88f48b9a8b
Alex Topper <42527102+Pedestria@users.noreply.github.com>
2021-08-31 11:18:43 -0400

    Omegaslc c++ init

---
+++ omegasl/src/Lexer.h
@@ -0,0 +1,10 @@
+
+#ifndef OMEGASL_LEXER_H
+#define OMEGASL_LEXER_H
+
+
+namespace omegasl {
+    
+}
+
+#endif //OMEGASL_LEXER_H
---
+++ omegasl/src/main.cpp
@@ -0,0 +1,5 @@
+#include <omega-common/common.h>
+
+int main(int argc,char *argv[]){
+    return 0;
+};
